62e33c2553c72b94191b9981c068a894129bb05a
Jacz <jacknagle22@gmail.com>
2018-04-16 14:41:16 +1000

    Update cors.js

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,5 +1,4 @@
 module.exports = (req, res) => {
     res.set("Access-Control-Allow-Origin", "*");
-    res.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     return true;
 };
